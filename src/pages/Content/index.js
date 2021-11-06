cfa9fb437670ded97fb4264b0f8a62f782c6e001
Yong Ding <dingo.cn@gmail.com>
2021-11-06 16:30:57 -0400

    Add back the index changes

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,4 +1,3 @@
-
 import { ComputerVisionClient } from '@azure/cognitiveservices-computervision';
 import { ApiKeyCredentials } from '@azure/ms-rest-js';
 
@@ -32,34 +31,58 @@ function recognizedImage(imgTag) {
                 if(data.captions[0].text === 'text')
                 {
                     //Need to call OCR API to recognize the text
-                    recognizedText(url);
+                    recognizedText(imgTag);
                 }
-                appendAlt(imgTag, data);
+                else
+                {
+                  //get display text
+                  let altText = generateAltText(data.captions[0].text, data.captions[0].confidence);
+
+                  appendAlt(imgTag, altText);
+                }
             }
         })
       .catch(console.error);
 }
 
 function appendAlt(imgTag, altText) {
-
-  console.log(result);
-
-  imgTag.alt = generateAltText(altText);
-
-  console.log(imgTag.alt);
-
-//   filter for accuracy
-//   let accurateDesc = result.description.captions.filter((val, index) => {
-//     return 
-//   })
+  imgTag.alt = altText;
 }
 
-function generateAltText(data)
-{
-    return "";
+
+function generateAltText(data, confidence){
+  let ret = '';
+  if(confidence >= 0.40)
+    ret = data;
+  else if(confidence >= 0.2 && confidence < 0.4)
+    ret = "It probally is " + data;
+  else if(confidence >= 0.1 && confidence < 0.2)
+    ret = "It likely is " + data;
+  else
+    ret = "Image cannbe be recoginized.";
+
+  return ret;
 }
 
-function recognizedText(url)
+function recognizedText(imgTag)
 {
-  //Call OCR API
+  var url = imgTag.src;
+  computerVisionClient.recognizePrintedText(true, url, {language:"en"})
+  .then((result) => {
+    if(result.regions?.length > 0)
+    {
+      let text = "";
+      if(result.regions[0].lines?.length > 0)
+      {
+        result.regions[0].lines[0].words.forEach(element => {
+          text += element.text + " ";
+        });
+
+        //get display text
+        let altText = generateAltText(text, 1);
+
+        appendAlt(imgTag, altText);
+      }
+    }
+  });
 }
